eb6e478e345d8541e3b76d35482053657facb7d2
BanuAshik <abrosebanu8@gmail.com>
2024-08-18 17:17:48 +0400

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,21 @@
+package homeassignment;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version =91.0F;
+	    String developer ="Google";
+	    boolean beta = false;
+	    int releaseYear= 2008;
+	    char shortcutKey  = 'C';
+	    System.out.println("Version"+ "=" + version);
+	    System.out.println("developer"+ "=" + developer);
+	    System.out.println("beta"+ "=" + beta);
+	    System.out.println("releaseYear"+"="+ releaseYear);
+	    System.out.println("shortcutKey"+ "=" +shortcutKey);
+	    
+
+	}
+
+}
